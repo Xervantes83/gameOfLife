fa61aa255ff6b2efae7e82f28b4bbc12229dc5af
jfinnegan3 <jfinnegan3@gatech.edu>
2015-07-23 18:41:18 -0400

    Revert "Revert "Temp Commit""
    This reverts commit e7e384e54f180b930430149c326c24fc92e4cfad.
    
    Conflicts:
    	.idea/workspace.xml
    	src/sample/SwitchNames.java
    	src/sample/startScreen.fxml

---
+++ src/sample/SwitchNames.java
@@ -0,0 +1,16 @@
+package sample;
+
+/**
+ * Created by Jake on 7/23/2015.
+ */
+import java.io.*
+public class SwitchNames {
+    public static void main (String[] args) {
+        try {
+            File fxmlFile = new File("gameboard.fxml");
+            file.
+        } catch ( IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
